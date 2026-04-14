f1bce42caa1eb7759488875d0fa94417f69fea2a
LRBDeveloper <146123222+LRBDeveloper@users.noreply.github.com>
2026-04-14 10:19:39 +0200

    .

--- src/app/screens/chatPage.tsx
+++ src/app/screens/chatPage.tsx
@@ -7,6 +7,7 @@ import MessageComponent from '../components/messageComponent';
 import { useAuth } from '../contexts/AuthContext';
 import { getChatFromProblemId } from '../services/chatService';
 import useMessageViewModel from '../services/messageViewModel';
+import useStompMessageService from '../services/stompMessageService';
 import { Message } from '../types/Message';
 
 
@@ -24,7 +25,10 @@ export default function ChatScreen(){
     const {messages, setMessages, fetchMessagesFromChatId} = useMessageViewModel();
     const {isAuthenticated, user, token} = useAuth();
     const flatListRef = useRef<FlatList<Message>>(null);
+    const {initiateConnection, activate, disconnect, publishMessageWithHeaders} = useStompMessageService();
 
+    /** Automatically scroll down upon new incoming messages
+     */
     useEffect(() => {
         if(flatListRef.current && messages.length>0){
             flatListRef.current.scrollToEnd({animated: true})
@@ -32,110 +36,61 @@ export default function ChatScreen(){
     },[messages])
 
     function receiveMessage(message: {body: string}){
-
         const data = JSON.parse(message.body);
-
         const newMessage: Message = data;
-
         setMessages(prevMessages => [...prevMessages, newMessage]);
     }
 
-    //Initialize client
-    function initiateConnection(chatId: number){
-
-        const client = new Client({
-            brokerURL: `${socketURL}/chat`,
-            connectHeaders:{Authorization: `Bearer ${token}`},
-            onConnect: async () => {
-                client.subscribe(`/topic/messages/${chatId}`, (message) => {
-                    receiveMessage(message);
-                });
-                client.subscribe(`/user/queue/errors`, (error) => {
-                    console.log(error.body)
-                });
-
-                try{
-                    await fetchMessagesFromChatId(chatId);
-                }catch(err){
-                    //Don't do anything for now
-                    console.log(err);
-                }
-                setConnected(true);
+    function handleSendMessage(){
+        try{
+            var message_req = publishMessageWithHeaders(chatId,messageInput);
+            if(message_req){
+                setMessageInput('');
             }
-        })
-        stompClient.current=client;
-        return;
-    }
-
-    if(stompClient!=null){
-        stompClient.onWebSocketError = (error) => {
-            console.log('error' + error);
+        }catch(err){
+            alert(err)
         }
     }
 
-    function connect(){
-        if(stompClient.current == null){
-            initiateConnection();
-        }
-        
-        stompClient.current.activate();
-    }
-
-    function disconnect(){
-        if(!stompClient.current){
-            return;
-        }
-        stompClient.current.deactivate();
-        setConnected(false);
-    }
-
-    // Send a message
-    const sendMessage = async () => {
-
-        if(!isAuthenticated || !token){
-            return alert("Unable to send messages while not signed in")
-        }
-
-        if(messageInput==''){
-            return;
-        }
-
-        if (!stompClient.current || !stompClient.current.connected) {
-            console.error("Not connected to WebSocket!");
-            return;
-        }
-
-        const headers: Record<string, string> = {};
-        headers.Authorization = `Bearer ${token}`;
-
-        stompClient.current.publish({
-            destination: `/app/chat/${chatId}`,
-            body: JSON.stringify({message: messageInput }),
-            headers: headers
-        });
-        setMessageInput('');
-    };
-
+    /** Automatically fetch messages and initialize STOMP connection on render
+     */
     useEffect(() => {
 
-
         const autoLoadAndConnect = async() => {
+            //Get chatId
             const _chatId: number = await getChatFromProblemId(problemId);
             await setChatId(_chatId);
-            await initiateConnection(_chatId);
-            await connect();
+
+            //Initiate STOMP handshake
+            await initiateConnection(
+                _chatId,
+                receiveMessage,
+                async () => {
+                    try{
+                        await fetchMessagesFromChatId(_chatId);
+                    }catch(err){
+                        //Don't do anything for now
+                        console.log(err);
+                    }
+                    setConnected(true);
+                }
+            );
+
+            //Activate stomp connection
+            var activate_result = activate();
+            console.log(activate_result)
         }
 
         autoLoadAndConnect();
 
         //Cleanup connection on leaving the chat page
         return() => {
-            disconnect();
+            var disconnectResult = disconnect();
+            setConnected(!disconnectResult);
         }
     },[])
 
     const isSendAble = connected && isAuthenticated && token && messageInput!="";
-    console.log("connected: " + connected + "\n" + "token:" + token)
 
     return(
         <View>
@@ -155,7 +110,7 @@ export default function ChatScreen(){
                 </TextInput>
                 <TouchableOpacity
                     style={(!isSendAble? chatStyle.messageSendBtnDisabled: chatStyle.messageSendBtn)}
-                    onPress={sendMessage}
+                    onPress={handleSendMessage}
                     disabled={!isSendAble}>
                     <Feather color={(!isSendAble? 'lightgray': '#5e9cff')} name='send' size={24}></Feather>
                 </TouchableOpacity>
